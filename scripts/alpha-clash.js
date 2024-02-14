ee928b86999921f2f22986c943c158246f07dc4a
AdnaNation <azizulislamadnan@gmail.com>
2024-02-14 13:09:56 +0600

    second  page js added

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,8 +1,26 @@
-function play(){
-    const homeSection = document.getElementById('home-screen');
-    homeSection.classList.add('hidden');
+// function play(){
+//     const homeSection = document.getElementById('home-screen');
+//     homeSection.classList.add('hidden');
 
-    const playGroundSection = document.getElementById('play-ground');
-    playGroundSection.classList.remove('hidden');
+//     const playGroundSection = document.getElementById('play-ground');
+//     playGroundSection.classList.remove('hidden');
     
+// }
+
+function continueGame(){
+    const alphabet = getARandomAlphabet();
+     console.log('your random alphabet', alphabet)
+
+    //  set randomly genarated alphabet to the screen
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+    // set backgroundColor
+    setBacgroundColorById(alphabet);
+}
+
+function play(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame()
 }
